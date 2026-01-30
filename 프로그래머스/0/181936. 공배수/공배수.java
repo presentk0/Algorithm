cae8c4dc76d6a228f7400b1097dbdc3b61e88135
presentk <137313138+presentk0@users.noreply.github.com>
2026-01-30 22:13:55 +0900

    [level 0] Title: 공배수, Time: 0.02 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -3,9 +3,7 @@ class Solution {
         int answer = 0;
         if(number%n==0 && number%m==0) {
             answer=1;
-        } else {
-            answer=0;
-        }
+        } 
         return answer;
     }
 }
